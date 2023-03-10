989417a4f4eb59f73a61bc4e34f6cd0b08124ea6
sotojuan2 <soto.juan@gmail.com>
2023-03-10 20:38:09 +0100

    tomas bien

--- unit_tests/V000001__initialize_order_events_stream_output.sql
+++ unit_tests/V000001__initialize_order_events_stream_output.sql
@@ -1,9 +1,9 @@
 {
   "outputs": [
-    {"topic": "S1", "timestamp": 0, "key": 0, "value": {"ORDERUNITS": 2.0, "CASE_RESULT": "medium"}},
-    {"topic": "S1", "timestamp": 0, "key": 100, "value": {"ORDERUNITS": 4.0, "CASE_RESULT": "large"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 6.0, "CASE_RESULT": "large"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 3.0, "CASE_RESULT": "medium"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 1.0, "CASE_RESULT": "small"}}
+    {"topic": "S1", "timestamp": 0, "key": 0, "value": {"P_ORDERUNITS": 2.0, "CASE_RESULT": "medium"}},
+    {"topic": "S1", "timestamp": 0, "key": 100, "value": {"P_ORDERUNITS": 4.0, "CASE_RESULT": "large"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"P_ORDERUNITS": 6.0, "CASE_RESULT": "large"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"P_ORDERUNITS": 3.0, "CASE_RESULT": "medium"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"P_ORDERUNITS": 1.0, "CASE_RESULT": "small"}}
   ]
 }
