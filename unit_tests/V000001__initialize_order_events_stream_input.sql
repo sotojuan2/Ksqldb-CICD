01ee35fb18714781350dcea500cfd9f4659d286a
sotojuan2 <soto.juan@gmail.com>
2023-03-10 15:37:24 +0100

    unit test

---
+++ unit_tests/V000001__initialize_order_events_stream_input.sql
@@ -0,0 +1,8 @@
+{
+  "inputs": [
+          {"topic": "STM_DEPARTMENTS_RAW", "timestamp": 1563444000000, "value": {"DEPT_NO": "d001", "DEPT_NAME": "Marketing"}, "key": "d001"},
+          {"topic": "STM_DEPARTMENTS_RAW", "timestamp": 1563444000000, "value": {"DEPT_NO": "d002", "DEPT_NAME": "Claims"}, "key": "d002"},
+          {"topic": "STM_DEPARTMENTS_RAW", "timestamp": 1563444000000, "value": {"DEPT_NO": "d003", "DEPT_NAME": "Accounting"}, "key": "d003"},
+          {"topic": "STM_DEPARTMENTS_RAW", "timestamp": 1563444000000, "value": {"DEPT_NO": "d004", "DEPT_NAME": "Information Technology"}, "key": "d004"}
+        ]
+}
---
+++ unit_tests/V000001__initialize_order_events_stream_output.sql
@@ -0,0 +1,8 @@
+{
+  "outputs": [
+          {"topic": "STM_DEPARTMENTS_WITH_KEY", "timestamp": 1563444000000, "value": {"DEPT_NO": "D001", "DEPT_NAME": "MARKETING"}, "key": "D001"},
+          {"topic": "STM_DEPARTMENTS_WITH_KEY", "timestamp": 1563444000000, "value": {"DEPT_NO": "D002", "DEPT_NAME": "CLAIMS"}, "key": "D002"},
+          {"topic": "STM_DEPARTMENTS_WITH_KEY", "timestamp": 1563444000000, "value": {"DEPT_NO": "D003", "DEPT_NAME": "ACCOUNTING"}, "key": "D003"},
+          {"topic": "STM_DEPARTMENTS_WITH_KEY", "timestamp": 1563444000000, "value": {"DEPT_NO": "D004", "DEPT_NAME": "INFORMATION TECHNOLOGY"}, "key": "D004"}
+        ]
+}
