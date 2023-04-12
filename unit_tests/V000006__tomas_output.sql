2aaf58223f50ddfd505a4eb022aec1d7a1567c72
sotojuan2 <soto.juan@gmail.com>
2023-04-12 17:36:50 +0200

    prueba de pr

--- unit_tests/V000006__tomas_output.sql
+++ unit_tests/V000006__tomas_output.sql
@@ -1,9 +1,9 @@
 {
   "outputs": [
-    {"topic": "S1", "timestamp": 0, "key": 0, "value": {"ORDERUNITS_S": 2.0, "CASE_RESULT": "medium"}},
-    {"topic": "S1", "timestamp": 0, "key": 100, "value": {"ORDERUNITS_S": 4.0, "CASE_RESULT": "large"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS_S": 6.0, "CASE_RESULT": "large"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS_S": 3.0, "CASE_RESULT": "medium"}},
-    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS_S": 1.0, "CASE_RESULT": "small"}}
+    {"topic": "S1", "timestamp": 0, "key": 0, "value": {"ORDERUNITS": 2.0, "CASE_RESULT": "medium"}},
+    {"topic": "S1", "timestamp": 0, "key": 100, "value": {"ORDERUNITS": 4.0, "CASE_RESULT": "large"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 6.0, "CASE_RESULT": "large"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 3.0, "CASE_RESULT": "medium"}},
+    {"topic": "S1", "timestamp": 0, "key": 101, "value": {"ORDERUNITS": 1.0, "CASE_RESULT": "small"}}
   ]
 }
