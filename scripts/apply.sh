009960c341cb248b069889d8ff3cc15b22fbf4d6
sotojuan2 <soto.juan@gmail.com>
2023-05-08 17:26:34 +0200

    apply anda que meter una A

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -5,7 +5,7 @@ echo "Ini apply"
 DIR=`pwd`
 echo "el directorio ${DIR}"
 OUTPUT=`ksql-migrations -c ksql-migrations.properties applay -a`
-ksql-migrations -c ksql-migrations.properties applay -a | grep -q Successfully
+ksql-migrations -c ksql-migrations.properties apply -a | grep -q Successfully
 if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; 
 else  echo "UNIT TEST fail - " ${OUTPUT} 
         exit 1 ; 
