5049a733f2e4bbb92a9a96d6b3bd9d5223a405f1
sotojuan2 <soto.juan@gmail.com>
2023-05-08 12:32:26 +0200

    cambio script

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 
-
+echo "Ini apply"
 ksql-migrations -c ../ksql-migrations.properties applay -a | grep -q Successfully
-if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; else exit 1 ; fi
+if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; else  echo "UNIT TEST fail" exit 1 ; fi
 echo "TEST DONE"
 exit 0
