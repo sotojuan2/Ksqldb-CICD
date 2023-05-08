2e3cc816ca5a1055f1a6ab1571a039bfb1f61b37
sotojuan2 <soto.juan@gmail.com>
2023-05-08 18:23:10 +0200

    nuevos cambios

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -5,9 +5,9 @@ echo "Ini apply"
 DIR=`pwd`
 echo "el directorio ${DIR}"
 OUTPUT=`ksql-migrations -c ksql-migrations.properties apply -a`
-ksql-migrations -c ksql-migrations.properties apply -a | grep -q Successfully
-if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; 
-else  echo "UNIT TEST fail - " ${OUTPUT} 
+echo "${OUTPUT}" | grep -q Successfully
+if [ $? -eq 0 ] ; then echo "UNIT TEST DONE  ${OUTPUT}" ; 
+else  echo "UNIT TEST fail -  ${OUTPUT}" 
         exit 1 ; 
 fi
 echo "TEST DONE"
