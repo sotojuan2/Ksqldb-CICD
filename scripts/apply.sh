9b049f5911a2e0d67f4be72d3167b7b747b6ccf0
sotojuan2 <soto.juan@gmail.com>
2023-05-08 12:47:51 +0200

    prueba2

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -2,7 +2,8 @@
 
 
 echo "Ini apply"
-ksql-migrations -c ../ksql-migrations.properties applay -a | grep -q Successfully
-if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; else  echo "UNIT TEST fail" exit 1 ; fi
+OUTPUT="$(ksql-migrations -c ../ksql-migrations.properties applay -a)"
+echo "${OUTPUT}" | grep -q Successfully
+if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; else  echo "UNIT TEST fail - " ${OUTPUT} exit 1 ; fi
 echo "TEST DONE"
 exit 0
