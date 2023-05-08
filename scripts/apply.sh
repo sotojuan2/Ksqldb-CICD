070148325e26f4f4364d4cb907108e3d8af3833c
sotojuan2 <soto.juan@gmail.com>
2023-05-08 13:04:53 +0200

    Update apply.sh

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,7 +4,7 @@
 echo "Ini apply"
 DIR="$('pwd')"
 echo "el directorio ${DIR}"
-OUTPUT="$('ksql-migrations -c ../ksql-migrations.properties applay -a')"
+OUTPUT="$('ksql-migrations -c ksql-migrations.properties applay -a')"
 echo "${OUTPUT}" | grep -q Successfully
 if [ $? -eq 0 ] ; then echo "UNIT TEST DONE" ; 
 else  echo "UNIT TEST fail - " ${OUTPUT} 
