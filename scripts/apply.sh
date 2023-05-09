960595c632c9a565c659beeb15bfea5378c9211c
sotojuan2 <soto.juan@gmail.com>
2023-05-09 15:21:37 +0200

    No eligible migrations found

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -5,10 +5,13 @@ echo "Ini apply"
 DIR=`pwd`
 echo "el directorio ${DIR}"
 OUTPUT=`ksql-migrations -c ksql-migrations.properties apply -a --dry-run`
-echo "${OUTPUT}" | grep -q Successfully
-if [ $? -eq 0 ] ; then echo "UNIT TEST DONE  ${OUTPUT}" ; 
+#echo "${OUTPUT}" | grep -q Successfully
+echo "${OUTPUT}" | grep -q 'No eligible migrations found\|pattern2'
+if [ $? -eq 0 ] ; 
+    then echo "ERROR  ${OUTPUT}" 
+    exit 1; 
 else  echo "UNIT TEST fail -  ${OUTPUT}" 
-        exit 1 ; 
+        exit 0 ; 
 fi
 echo "TEST DONE"
 exit 0
