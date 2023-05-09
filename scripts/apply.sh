47ed53967d0713534eb92e19a98113bddb651799
sotojuan2 <soto.juan@gmail.com>
2023-05-09 15:14:41 +0200

    dry-run

--- scripts/apply.sh
+++ scripts/apply.sh
@@ -4,7 +4,7 @@
 echo "Ini apply"
 DIR=`pwd`
 echo "el directorio ${DIR}"
-OUTPUT=`ksql-migrations -c ksql-migrations.properties apply -a`
+OUTPUT=`ksql-migrations -c ksql-migrations.properties apply -a --dry-run`
 echo "${OUTPUT}" | grep -q Successfully
 if [ $? -eq 0 ] ; then echo "UNIT TEST DONE  ${OUTPUT}" ; 
 else  echo "UNIT TEST fail -  ${OUTPUT}" 
