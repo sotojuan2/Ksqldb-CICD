84028f018ecfc4fc565c1babf3208fc399512f09
sotojuan2 <soto.juan@gmail.com>
2023-05-18 17:09:10 +0200

    Update V000003__user_pageviews_input.sql

--- unit_tests/V000003__user_pageviews_input.sql
+++ unit_tests/V000003__user_pageviews_input.sql
@@ -1,10 +1,10 @@
 {
   "inputs": [
     {"topic": "test_topic", "timestamp": 0, "key": "b", "value": {"viewtime": 21992,"userid": "User_5","pageid": "Page_80"}},
+    {"topic": "users2", "timestamp": 0, "key": "B", "value": {"registertime": 1506621975471,"userid": "User_5","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "test_topic", "timestamp": 0, "key": "c", "value": {"viewtime": 21993,"userid": "User_6","pageid": "Page_81"}},
     {"topic": "users2", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_4","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "test_topic", "timestamp": 0, "key": "a", "value": {"viewtime": 21991,"userid": "User_4","pageid": "Page_80"}},
-    {"topic": "users2", "timestamp": 0, "key": "B", "value": {"registertime": 1506621975471,"userid": "User_5","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "users2", "timestamp": 0, "key": "C", "value": {"registertime": 1506621975472,"userid": "User_6","regionid": "Region_6","gender": "FEMALE"}}
   ]
 }
