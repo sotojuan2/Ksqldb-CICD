c1956209e0e704cb5f2a00585b0911fec016f53c
sotojuan2 <soto.juan@gmail.com>
2023-05-18 11:24:03 +0200

    comillas dobles

--- unit_tests/V000001__PAGEVIEWS_STREAM_output.sql
+++ unit_tests/V000001__PAGEVIEWS_STREAM_output.sql
@@ -1,7 +1,7 @@
 {
   "outputs": [
-    {"topic": "test_topic", "timestamp": 0, "key": 'a', "value": {"viewtime": 21991,"userid": "User_4","pageid": "Page_80"}},
-    {"topic": "test_topic", "timestamp": 0, "key": 'b', "value": {"viewtime": 21992,"userid": "User_5","pageid": "Page_80"}},
-    {"topic": "test_topic", "timestamp": 0, "key": 'c', "value": {"viewtime": 21993,"userid": "User_6","pageid": "Page_81"}}
+    {"topic": "test_topic", "timestamp": 0, "key": "a", "value": {"viewtime": 21991,"userid": "User_4","pageid": "Page_80"}},
+    {"topic": "test_topic", "timestamp": 0, "key": "b", "value": {"viewtime": 21992,"userid": "User_5","pageid": "Page_80"}},
+    {"topic": "test_topic", "timestamp": 0, "key": "c", "value": {"viewtime": 21993,"userid": "User_6","pageid": "Page_81"}}
   ]
 }
