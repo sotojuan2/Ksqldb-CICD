53178530e4063f2adefef5316e0cdc07750c9786
sotojuan2 <soto.juan@gmail.com>
2023-05-18 17:36:57 +0200

    MAS CAMBIOS

--- unit_tests/V000003__user_pageviews_input.sql
+++ unit_tests/V000003__user_pageviews_input.sql
@@ -1,10 +1,10 @@
 {
   "inputs": [
     {"topic": "users2", "timestamp": 0, "key": "User_5", "value": {"registertime": 1506621975471,"userid": "User_5","regionid": "Region_6","gender": "FEMALE"}},
-    {"topic": "users2", "timestamp": 0, "key": "User_4", "value": {"registertime": 1506621975470,"userid": "User_4","regionid": "Region_6","gender": "FEMALE"}},
+    {"topic": "users2", "timestamp": 0, "key": "A", "value": {"registertime": 1506621975470,"userid": "User_4","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "users2", "timestamp": 0, "key": "User_6", "value": {"registertime": 1506621975472,"userid": "User_6","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "test_topic", "timestamp": 0, "key": "User_5", "value": {"viewtime": 21992,"userid": "User_5","pageid": "Page_80"}},
-    {"topic": "test_topic", "timestamp": 0, "key": "User_6", "value": {"viewtime": 21993,"userid": "User_6","pageid": "Page_81"}},
-    {"topic": "test_topic", "timestamp": 0, "key": "a", "value": {"viewtime": 21991,"userid": "User_4","pageid": "Page_80"}}
+    {"topic": "test_topic", "timestamp": 0, "key": "User_4", "value": {"viewtime": 21991,"userid": "User_4","pageid": "Page_80"}},
+    {"topic": "test_topic", "timestamp": 0, "key": "User_6", "value": {"viewtime": 21993,"userid": "User_6","pageid": "Page_81"}}
   ]
 }
