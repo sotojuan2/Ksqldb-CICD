f493b188d7149d999f5de70da7af18f4bd26f578
sotojuan2 <soto.juan@gmail.com>
2023-05-18 15:30:48 +0200

    Update V000002__USERS_TABLE_output.sql

--- unit_tests/V000002__USERS_TABLE_output.sql
+++ unit_tests/V000002__USERS_TABLE_output.sql
@@ -1,7 +1,7 @@
 {
   "outputs": [
     {"topic": "USERS_OUT", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
-    {"topic": "USERS_OUT", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
-    {"topic": "USERS_OUT", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}}
+    {"topic": "USERS_OUT", "timestamp": 0, "key": "B", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
+    {"topic": "USERS_OUT", "timestamp": 0, "key": "B", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}}
   ]
 }
