54bc5a5f93184b4b8db6802e055796a1f5eb7876
sotojuan2 <soto.juan@gmail.com>
2023-05-18 17:44:51 +0200

    Update V000003__user_pageviews_input.sql

--- unit_tests/V000003__user_pageviews_input.sql
+++ unit_tests/V000003__user_pageviews_input.sql
@@ -1,6 +1,6 @@
 {
   "inputs": [
-    {"topic": "users2", "timestamp": 0, "key": "User_5", "value": {"registertime": 1506621975471,"userid": "User_5","regionid": "Region_6","gender": "FEMALE"}},
+    {"topic": "users2", "timestamp": 0,  "value": {"registertime": 1506621975471,"userid": "User_5","regionid": "Region_6","gender": "FEMALE", "id":"User_5"}},
     {"topic": "users2", "timestamp": 0, "key": "User_4", "value": {"registertime": 1506621975470,"userid": "User_4","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "users2", "timestamp": 0, "key": "User_6", "value": {"registertime": 1506621975472,"userid": "User_6","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "test_topic", "timestamp": 0, "key": "User_5", "value": {"viewtime": 21992,"userid": "User_5","pageid": "Page_80"}},
