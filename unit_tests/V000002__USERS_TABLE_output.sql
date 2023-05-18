fca1af0a29cbc69e9b26f482fb23622d27b3ff2c
sotojuan2 <soto.juan@gmail.com>
2023-05-18 15:04:02 +0200

    fix some errors

--- unit_tests/V000002__USERS_TABLE_output.sql
+++ unit_tests/V000002__USERS_TABLE_output.sql
@@ -2,6 +2,6 @@
   "outputs": [
     {"topic": "users2", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
     {"topic": "users2", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
-    {"topic": "users2", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}},
+    {"topic": "users2", "timestamp": 0, "key": "a", "value": {"registertime": 1506621975470,"userid": "User_1","regionid": "Region_6","gender": "FEMALE"}}
   ]
 }
