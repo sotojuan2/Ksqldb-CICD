f34fa2b423d02aa0ec2a8e2ac326ab3950aad7a0
Juan <soto.juan@gmail.com>
2023-06-19 15:20:57 +0200

    user 6

--- unit_tests/V000004__userPageRegion_output.sql
+++ unit_tests/V000004__userPageRegion_output.sql
@@ -1,6 +1,6 @@
 {
   "outputs": [
     {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_5", "value": {"pageid": 12,"regionid": "Region_8"}},
-    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_6", "value": {"USERID": "User_6","PAGEID": 14,"REGIONID": "Region_9"}}
+    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_6", "value": {"PAGEID": 14,"REGIONID": "Region_9"}}
   ]
 }
