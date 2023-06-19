c4038d0d4c605bcce860d4f4839725b9c6c596ae
Juan <soto.juan@gmail.com>
2023-06-19 15:16:50 +0200

    USERID

--- unit_tests/V000004__userPageRegion_input.sql
+++ unit_tests/V000004__userPageRegion_input.sql
@@ -1,6 +1,6 @@
 {
   "inputs": [
-    {"topic": "pageviews_region", "timestamp": 0, "key": "User_5", "value": {"registertime": 1506621975471,"userid": "User_5","pageid":12,"regionid": "Region_8"}},
+    {"topic": "pageviews_region", "timestamp": 0, "key": "User_5", "value": {"userid": "User_5","pageid":12,"regionid": "Region_8"}},
     {"topic": "pageviews_region", "timestamp": 0, "key": "User_4", "value": {"registertime": 1506621975470,"userid": "User_4","pageid":13,"regionid": "Region_6"}},
     {"topic": "pageviews_region", "timestamp": 0, "key": "User_6", "value": {"registertime": 1506621975472,"userid": "User_6","pageid":14,"regionid": "Region_9"}}
   ]
