756919608018b87c9386dc52b3e124df129e2f48
Juan <soto.juan@gmail.com>
2023-06-19 15:05:11 +0200

    New Entity
    des

---
+++ unit_tests/V000004__userPageRegion_output.sql
@@ -0,0 +1,6 @@
+{
+  "outputs": [
+    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_5", "value": {"USERID": "User_5","PAGEID": 12,"REGIONID": "Region_8"}},
+    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_6", "value": {"USERID": "User_6","PAGEID": 14,"REGIONID": "Region_9"}}
+  ]
+}
