2a12128b9f05405aafb4de34cf6a126e07d30fc7
sotojuan2 <soto.juan@gmail.com>
2023-06-20 17:50:23 +0200

    Delete V000005__pageviews_region_replace_output.sql

--- unit_tests/V000005__pageviews_region_replace_output.sql
+++
@@ -1,6 +0,0 @@
-{
-  "outputs": [
-    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_5", "value": {"pageid": 12,"regionid": "Region_8", "gender": "female"}},
-    {"topic": "USER_PAGEVIEWSREGION", "timestamp": 0, "key": "User_6", "value": {"PAGEID": 14,"REGIONID": "Region_9", "gender": "female"}}
-  ]
-}
